7f527d0e2602fb931434e8954abe9403fd14ce7f
BelfordZ <semantic-release-bot@martynus.net>
2022-10-29 00:06:15 +0000

    chore(release): add newly generated typings

--- src/lib.rs
+++ src/lib.rs
@@ -134,9 +134,7 @@ pub struct JSONRPCResponse {
 pub type JSONRPCBatchRequest = Vec<JSONRPCRequest>;
 /// JSONRPCBatchResponse
 ///
-/// The Server should respond with an Array containing the corresponding Response objects, after all of the batch Request objects have been processed. A Response object SHOULD exist for each Request object, except that there SHOULD NOT be any Response objects for notifications. The Server MAY process a batch rpc call as a set of concurrent tasks, processing them in any order and with any width of parallelism.
- The Response objects being returned from a batch call MAY be returned in any order within the Array. 
- If the batch rpc call itself fails to be recognized as an valid JSON or as an Array with at least one value, the response from the Server MUST be a single Response object. If there are no Response objects contained within the Response array as it is to be sent to the client, the server MUST NOT return an empty Array and should return nothing at all.
+/// The Server should respond with an Array containing the corresponding Response objects, after all of the batch Request objects have been processed. A Response object SHOULD exist for each Request object, except that there SHOULD NOT be any Response objects for notifications. The Server MAY process a batch rpc call as a set of concurrent tasks, processing them in any order and with any width of parallelism. The Response objects being returned from a batch call MAY be returned in any order within the Array. If the batch rpc call itself fails to be recognized as an valid JSON or as an Array with at least one value, the response from the Server MUST be a single Response object. If there are no Response objects contained within the Response array as it is to be sent to the client, the server MUST NOT return an empty Array and should return nothing at all.
 ///
 pub type JSONRPCBatchResponse = Vec<JSONRPCResponse>;
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
